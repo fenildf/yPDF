e88ddcc82a9dad49771853aac5dceb596865d245
高垚鑫 <gaoyaoxin@bfsu.edu.cn>
2018-05-10 22:39:34 +0800

    update

---
+++ yPDF_lite_en.py
@@ -0,0 +1,15 @@
+import pyperclip
+
+fo = open('yPDF.txt', 'w', encoding='UTF-8')
+fo.write(str(pyperclip.paste()))
+fo.close()
+
+fo = open('yPDF.txt', 'r', encoding='UTF-8')
+text = fo.read()
+fo.close()
+
+# Separate lines and delete line feeds.
+text = text.split('\n') # each string in "lines" list are without line feeds already! 
+text = ' '.join(text)
+pyperclip.copy(text)
+print('Saved to clipboard already! ' 'Text lenth: ' + str(len(text)) + '. ' + 'Current time: ' + datetime.datetime.now().strftime('%Y.%m.%d-%H:%M:%S')) + '. '
